55db2a1e5f052de34eeac219a06a3b6f8f323db6
iandemed <iandemed@gmail.com>
2020-08-15 18:15:16 -0400

    Prints all non-action and not stat information for a creature

--- script.js
+++ script.js
@@ -2,7 +2,57 @@
 /* This is the base api url to get all of the monsters in the
 Dungeons and Dragons 5th Edition SRD
 */
-const url = "https://www.dnd5eapi.co/api/monsters/"
+const url = "https://www.dnd5eapi.co/api/monsters"
 
 console.log("This is the api url: " + url)
 
+
+let creatureNames = ['bandit', 'aboleth']
+
+for (let i = 0; i < creatureNames.length; i++){
+    fetch(`${url}/${creatureNames[i]}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            console.log(data.name)
+            console.log(`${data.size} ${data.type} (${data.subtype}), ${data.alignment}`)
+            console.log(`Armor Class: ${data.armor_class}`)
+            console.log(`Hit Points: ${data.hit_points} (${data.hit_dice})`)
+            
+            for (let key in data.speed){
+                console.log(`Speed: ${data.speed[key]} (${key})`)
+            }
+
+            data.proficiencies.forEach(obj => {
+                console.log(`${obj.name} ${obj.value}`) 
+            });
+
+            data.proficiencies.forEach(obj => {
+                console.log(`${obj.name} +${obj.value}`) 
+            });
+            data.damage_vulnerabilities.forEach(obj => {
+                console.log(`${obj.name} +${obj.value}`) 
+            });
+            data.damage_resistances.forEach(obj => {
+                console.log(`${obj.name} +${obj.value}`) 
+            });
+            data.damage_immunities.forEach(obj => {
+                console.log(`${obj.name} +${obj.value}`) 
+            });
+            data.condition_immunities.forEach(obj => {
+                console.log(`${obj.name} +${obj.value}`) 
+            });
+
+            for (let key in data.senses){
+                console.log(`${data.senses[key]}`)
+            }
+
+            console.log(`Languages: ${data.languages}`)
+
+            if (data.special_abilities){
+                data.special_abilities.forEach(obj => {
+                    console.log(`${obj.name}. ${obj.desc}`)
+                })
+            }
+        })
+}
