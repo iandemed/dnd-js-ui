5ba4f0b29b6cff2383ff30be7ad90cc9a03400ca
iandemed <iandemed@gmail.com>
2020-08-14 23:55:57 -0400

    Added initial files for the project

---
+++ script.js
@@ -0,0 +1,8 @@
+
+/* This is the base api url to get all of the monsters in the
+Dungeons and Dragons 5th Edition SRD
+*/
+const url = "https://www.dnd5eapi.co/api/monsters/"
+
+console.log("This is the api url: " + url)
+
