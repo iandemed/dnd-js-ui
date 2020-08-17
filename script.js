867d23a792e384ff8cfb1904727f4bb3c5ac4ed8
iandemed <iandemed@gmail.com>
2020-08-17 10:00:48 -0400

    Begin programatically creating the rest of the character sheet

--- script.js
+++ script.js
@@ -16,14 +16,6 @@ console.log("This is the api url: " + url)
 //             console.log(data)
 //             console.log(data.name)
 
-//             // Initialize the stats variables
-//             let str = data.strength
-//             let dex = data.dexterity
-//             let con = data.constitution
-//             let int = data.intelligence
-//             let wis = data.wisdom
-//             let cha = data.charisma
-
 
 //             let typeLine = `${data.size} ${data.type}`
 
@@ -35,54 +27,14 @@ console.log("This is the api url: " + url)
 //             }
 
 //             console.log(typeLine)
-//             console.log(`Armor Class: ${data.armor_class}`)
-//             console.log(`Hit Points: ${data.hit_points} (${data.hit_dice}+${abilityModifier(con)})`)
-
-//             console.log(prepareSpeed(data.speed))
-
-//             /* Create strings for Saving Throws and skill proficiences */
-//             let savingThrows = "Saving Throws:"
-//             let skills = "Skills:"
-//             data.proficiencies.forEach(obj => {
-//                 strLength = obj.name.length
-
-//                 if(obj.name.includes("Saving Throw:")){
-//                     savingThrows += `${obj.name.substr(13,strLength)}+${obj.value},` 
-//                 }
-
-//                 if(obj.name.includes("Skill:")){
-//                     skills += `${obj.name.substr(6,strLength)}+${obj.value},`
-//                 }
-//             });
+//            
 
 
-//             let stLength = savingThrows.length
-//             let skLength = skills.length
-//             if (stLength > 16){
-//                 console.log(savingThrows.substr(0, stLength - 1))
-//             }
-//             if (skLength > 8){
-//                 console.log(skills.substr(0,skLength - 1))
-//             }
-
-//             console.log(prepareResistanceandImmunity("Damage Vulnerabilities", data.damage_vulnerabilities))
-//             console.log(prepareResistanceandImmunity("Damage Resistances", data.damage_resistances))
-//             console.log(prepareResistanceandImmunity("Damage Immunities", data.damage_immunities))
-//             console.log(prepareResistanceandImmunity("Condition Immunities", data.condition_immunities))
-//             console.log(prepareSenses(data.senses))
-
-//             console.log(`Languages: ${data.languages}`)
-
-//             if (data.special_abilities){
-//                 for (let i=0; i < data.special_abilities.length; i++){
-//                     console.log(prepareAbilities(data.special_abilities[i]))
-//                 }
-//             }
 //         })
 // }
 
 
-let creatures = ['bandit', 'skeleton']
+let creatures = ['bandit', 'skeleton', 'aboleth']
 let container = document.querySelector(".container")
 
 for (let i = 0; i < creatures.length; i++){
@@ -98,43 +50,41 @@ for (let i = 0; i < creatures.length; i++){
             
             container.appendChild(btn)
 
-            let statBlock = document.createElement("div")
-            statBlock.classList.add("monster-sheet")
-            statBlock.innerHTML =`<h1> I'm just testing this out right now </h1> <p>  Ability check charm concealment earth subtype fighter fire domain paralyzed range penalty rebuke undead rounding spell slot subject telepathy turning check vulnerability to energy. Ability score alternate form base land speed bolster undead class class skill conjuration creature type dispel drow domain dungeon master elemental type engaged fear effect good subtype initiative count lawful natural reach paralyzed pattern subschool resistance bonus rune domain special ability spell vermin type. </p>`
 
-            container.appendChild(statBlock)
+            let monsterSheet = document.createElement("div")
+            monsterSheet.classList.add("monster-sheet")
+            monsterSheet.innerHTML = " Animal domain battle grid charm class constrict copper piece critical roll death domain earth domain elf domain energy drained favored class helpless lawful lethal damage miniature figure modifier paralysis poison shadow subschool spell domain stable starvation time domain travel domain."
+
+            statBlock = createStatBlock(data)
+            vitalsBlock = createVitalsBlock(data)
+
+
+
+            // Add all of the created divs to the monster's character sheet
+            monsterSheet.appendChild(vitalsBlock)
+            monsterSheet.appendChild(statBlock)
+            
+            console.log(prepareProficiencies(data.proficiencies))
+
+            if (data.special_abilities){
+                abilitiesBlock = createAbilitiesBlock(data.special_abilities)
+                monsterSheet.appendChild(abilitiesBlock)
+            }
+
+            container.appendChild(monsterSheet)
 
             btn.addEventListener("click", (e) => {
-                let statBlock = e.target.nextElementSibling
-                console.log(statBlock)
+                let monsterSheet = e.target.nextElementSibling
+                console.log(monsterSheet)
         
-                if (statBlock.style.maxHeight){
-                    statBlock.style.maxHeight = null
+                if (monsterSheet.style.maxHeight){
+                    monsterSheet.style.maxHeight = null
                 } else{
-                    statBlock.style.maxHeight = `${statBlock.scrollHeight}px`
+                    monsterSheet.style.maxHeight = `${monsterSheet.scrollHeight}px`
                 }
             })
         })
 }
-
-
-let acc = document.querySelectorAll(".accordion")
-
-
-acc.forEach( (obj) => {
-
-    obj.addEventListener("click", (e) => {
-        let statBlock = e.target.nextElementSibling
-        console.log(statBlock)
-
-        if (statBlock.style.maxHeight){
-            statBlock.style.maxHeight = null
-        } else{
-            statBlock.style.maxHeight = `${statBlock.scrollHeight}px`
-        }
-    })
-
-})
 
 /*Helper function that mainly focuses on capitalizing the second word
 in passive perception */
@@ -160,20 +110,21 @@ function prepareSenses(senses){
 }
 
 function prepareSpeed(speed){
-   let speedString = "Speed:"
+   let speedString = "<strong> Speed </strong>"
 
+   
     for (let key in speed){
         if (key === "walk"){
-            speedString += ` ${speed[key]},`
+            speedString += ` ${speed[key]}`
         } else {
             /* The basline assumption is that creatures can walk just like
             the player characters. We have to specify if they are able to
             fly or swim at different speeds */
-            speedString += ` ${key} ${speed[key]},`
+            speedString += `, ${key} ${speed[key]}`
         }
     }
    
-    return speedString.substring(0, speedString.length-1)
+    return pTagWrap(speedString)
 }
 
 /* Returns the ability score modifier for any ability that we put in to
@@ -224,10 +175,121 @@ function prepareAbilities(obj){
 
 
     if (usage){
-        str = `${obj.name} (${usage.times} ${usage.type}). ${obj.desc}`
+        str = `<p><strong>${obj.name} (${usage.times} ${usage.type}).</strong></p> <p>${obj.desc}</p>`
     } else{
-        str = `${obj.name}. ${obj.desc}` 
+        str = `<p><strong>${obj.name}.</strong></p> <p>${obj.desc}</p>` 
     }
 
     return str
 }
+
+function pTagWrap(str){
+    return `<p>${str}</p>`
+}
+
+function prepareStatRow(array){
+    for(i = 0; i < array.length; i++){
+        array[i] = pTagWrap(array[i])
+    }
+
+    return array.join(" ")
+}
+
+
+function createStatBlock(data){
+
+    // Initialize the stats variables
+    let statsArray = []    
+    statsArray[0] = data.strength
+    statsArray[1] = data.dexterity
+    statsArray[2] = data.constitution
+    statsArray[3] = data.intelligence
+    statsArray[4] = data.wisdom
+    statsArray[5] = data.charisma
+
+    let statsModArray = statsArray.map(abilityModifier)
+    let statsStrArray = ["STR", "DEX", "CON", "INT", "WIS", "CHA"]
+
+    let statsBlock = document.createElement("div")
+    statsBlock.classList.add("stat-block")
+
+    statsBlock.innerHTML =  prepareStatRow(statsStrArray) + prepareStatRow(statsArray) + prepareStatRow(statsModArray)
+    console.log(statsBlock.innerHTML)
+
+
+    return statsBlock
+}
+
+function createVitalsBlock(data){
+
+    let vitalsBlock = document.createElement("div")
+    let con = data.constitution
+
+
+    let ac = pTagWrap(`<strong> Armor Class </strong> ${data.armor_class}`)
+    let hp = pTagWrap(`<strong> Hit Points </strong> ${data.hit_points} (${data.hit_dice}+${abilityModifier(con)}`)
+    
+    vitalsBlock.innerHTML = ac + hp + prepareSpeed(data.speed)
+
+    return vitalsBlock
+}
+
+function createAbilitiesBlock(special_abilities){
+
+    let abilitiesBlock = document.createElement("div")
+
+    for (let i=0; i < special_abilities.length; i++){
+        abilitiesBlock.innerHTML += prepareAbilities(special_abilities[i])
+    }
+
+    return abilitiesBlock
+}
+
+
+
+/* Create strings for Saving Throws and skill proficiences. Characters and monsters in
+Dungeons and Dragons gain a bonus to any roll that they are proficient in to represent
+their increased skill compared to a layman. proficiencies are saved as an array of objects
+with the objects names being strings we need to parse for more information*/
+function prepareProficiencies(proficiencies){
+            let savingThrows = "<strong> Saving Throws </strong>"
+            let skills = "<strong> Skills </strong>"
+
+            let str = ""
+
+            proficiencies.forEach(obj => {
+                strLength = obj.name.length
+
+                if(obj.name.includes("Saving Throw:")){
+                    savingThrows +=`${obj.name.substr(13,strLength)}+${obj.value},` 
+                }
+
+                if(obj.name.includes("Skill:")){
+                    skills += `${obj.name.substr(6,strLength)}+${obj.value},`
+                }
+            });
+            
+            /* Monsters do not always have both skill and saving throw proficiencies,
+             as a result we need to determine if a specific monster has them and
+             include them as appropriate.
+             
+             Currently I use a naive approach of measuring the length of the strings
+             I intialized at the start of the function */
+            let stLength = savingThrows.length
+            let skLength = skills.length
+            if (stLength > 33){
+                str += pTagWrap(savingThrows.substr(0, stLength - 1))
+            }
+            if (skLength > 26){
+                str += pTagWrap(skills.substr(0,skLength - 1))
+            }
+
+            return str
+}
+//             console.log(prepareResistanceandImmunity("Damage Vulnerabilities", data.damage_vulnerabilities))
+//             console.log(prepareResistanceandImmunity("Damage Resistances", data.damage_resistances))
+//             console.log(prepareResistanceandImmunity("Damage Immunities", data.damage_immunities))
+//             console.log(prepareResistanceandImmunity("Condition Immunities", data.condition_immunities))
+//             console.log(prepareSenses(data.senses))
+
+//             console.log(`Languages: ${data.languages}`)
